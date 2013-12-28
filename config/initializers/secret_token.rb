93894ec6bf7d2c632b4cfbc47928ffea3d891496
Joe Nelson <cred+github@begriffs.com>
2013-12-28 14:32:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ipilot::Application.config.secret_key_base = 'f5181141ae89c8a594d3a6efb2ef03877e10804e854dd17d353b4992154bbeecec2e260317ccc473b0375539796fce7e293d5f44ba2d04b07b7b2e98d609c2a0'
